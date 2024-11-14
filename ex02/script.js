438b04d2dafd5f776782e09f183db005f931709d
_603395 <603395@buchananschools.com>
2024-11-14 11:38:22 -0500

    Finished each task

--- ex02/script.js
+++ ex02/script.js
@@ -1,14 +1,25 @@
 // Write your Task 1 code inside this function!
 function task1() {
   console.log("Running task 1");
+  // 1. Target the div
+  let target = document.querySelector("#text-content")
+  console.log(target)
+  // 2. Set Inner Text to something new
+  target.innerText = "Something new"
 }
 
 // Write your task 2 code inside this function!
 function task2() {
   console.log("Running task 2");
+  let target = document.querySelector("#task2")
+  console.log(target)
+  target.classList.toggle("inverted")
 }
 
 // Write your task 3 code inside this function!
 function task3() {
   console.log("Running task 3");
+  let target = document.querySelector("#list")
+  console.log(target)
+  target.appendChild(document.createElement('li')).textContent = "do it again"
 }
